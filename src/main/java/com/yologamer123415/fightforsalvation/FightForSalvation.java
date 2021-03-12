0de58438d225c7b05f9f1619ac74f259c7662b83
SBDeveloper <stijnbannink40@gmail.com>
2021-03-12 11:22:12 +0100

    Added start code and configuration

--- src/main/java/com/yologamer123415/fightforsalvation/FightForSalvation.java
+++ src/main/java/com/yologamer123415/fightforsalvation/FightForSalvation.java
@@ -3,13 +3,20 @@ package com.yologamer123415.fightforsalvation;
 import nl.han.ica.oopg.engine.GameEngine;
 
 public class FightForSalvation extends GameEngine {
+    public static void main(String[] args) {
+        FightForSalvation ffs = new FightForSalvation();
+
+        // Run the sketch.
+        ffs.runSketch();
+    }
+
     @Override
     public void setupGame() {
-
+        //TODO Run code on setup...
     }
 
     @Override
     public void update() {
-
+        //TODO Run code on update...
     }
 }
