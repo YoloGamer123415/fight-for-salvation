fb2d8c1aec467fcca77f38da450f663d8442ab34
SBDeveloper <stijnbannink40@gmail.com>
2021-03-12 11:43:06 +0100

    Readded main()

--- src/main/java/com/yologamer123415/fightforsalvation/FightForSalvation.java
+++ src/main/java/com/yologamer123415/fightforsalvation/FightForSalvation.java
@@ -3,13 +3,19 @@ package com.yologamer123415.fightforsalvation;
 import nl.han.ica.oopg.engine.GameEngine;
 
 public class FightForSalvation extends GameEngine {
+	public static void main(String[] args) {
+		FightForSalvation ffs = new FightForSalvation();
+
+		ffs.runSketch();
+	}
+
 	@Override
 	public void setupGame() {
-
+		//TODO Implement...
 	}
 
 	@Override
 	public void update() {
-
+		//TODO Implement...
 	}
 }
