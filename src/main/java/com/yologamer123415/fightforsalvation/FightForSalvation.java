8d8d293548b012544e161eef894f45d97a2fd170
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-03-12 11:26:55 +0100

    Changed some ide settings

--- src/main/java/com/yologamer123415/fightforsalvation/FightForSalvation.java
+++ src/main/java/com/yologamer123415/fightforsalvation/FightForSalvation.java
@@ -3,13 +3,13 @@ package com.yologamer123415.fightforsalvation;
 import nl.han.ica.oopg.engine.GameEngine;
 
 public class FightForSalvation extends GameEngine {
-    @Override
-    public void setupGame() {
+	@Override
+	public void setupGame() {
 
-    }
+	}
 
-    @Override
-    public void update() {
+	@Override
+	public void update() {
 
-    }
+	}
 }
