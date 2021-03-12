88ab7a1f960a0a55aad8fd3a1f6a23d46e9f088c
SBDeveloper <stijnbannink40@gmail.com>
2021-03-12 11:11:53 +0100

    Added code base

---
+++ src/main/java/com/yologamer123415/fightforsalvation/FightForSalvation.java
@@ -0,0 +1,15 @@
+package com.yologamer123415.fightforsalvation;
+
+import nl.han.ica.oopg.engine.GameEngine;
+
+public class FightForSalvation extends GameEngine {
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
