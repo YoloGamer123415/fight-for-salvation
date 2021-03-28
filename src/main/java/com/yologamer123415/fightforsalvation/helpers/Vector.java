6575588f9850b229d656499cca3be9fc6eedd5c6
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-03-28 17:14:33 +0200

    Implemnted Vector class

--- src/main/java/com/yologamer123415/fightforsalvation/helpers/Vector.java
+++ src/main/java/com/yologamer123415/fightforsalvation/helpers/Vector.java
@@ -1,8 +1,41 @@
 package com.yologamer123415.fightforsalvation.helpers;
 
 public class Vector {
-	private float angle;
 	private float x;
 	private float y;
-	private float length;
+	private float angle = 0;
+	private float length = 0;
+
+	public Vector(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Vector(float x, float y, float angle) {
+		this(x, y);
+
+		this.angle = angle;
+	}
+
+	public Vector(float x, float y, float angle, float length) {
+		this(x, y, angle);
+
+		this.length = length;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public float getAngle() {
+		return angle;
+	}
+
+	public float getLength() {
+		return length;
+	}
 }
