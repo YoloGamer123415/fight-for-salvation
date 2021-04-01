bcb35d384d027292f495449a1bfae0e6de8d43d0
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-04-01 12:53:04 +0200

    Every time a Monster dies, it adds a random amount of essence to the player

--- src/main/java/com/yologamer123415/fightforsalvation/monsters/Monster.java
+++ src/main/java/com/yologamer123415/fightforsalvation/monsters/Monster.java
@@ -2,6 +2,7 @@ package com.yologamer123415.fightforsalvation.monsters;
 
 import com.yologamer123415.fightforsalvation.FightForSalvation;
 import com.yologamer123415.fightforsalvation.helpers.CollidingHelper;
+import com.yologamer123415.fightforsalvation.helpers.Rarity;
 import com.yologamer123415.fightforsalvation.helpers.Vector;
 import com.yologamer123415.fightforsalvation.object.FlammableSpriteObject;
 import com.yologamer123415.fightforsalvation.player.Player;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Monster extends FlammableSpriteObject implements ICollidableWithTiles {
+	protected final Rarity rarity;
 	protected Weapon weapon;
 
 	private final boolean movingX;
@@ -29,6 +31,7 @@ public class Monster extends FlammableSpriteObject implements ICollidableWithTil
 	public Monster(Sprite sprite, int hp) {
 		super(sprite, 10, 2);
 		this.hp = hp;
+		this.rarity = Rarity.getRandomRarity();
 
 		final Random rand = new Random();
 
@@ -42,9 +45,29 @@ public class Monster extends FlammableSpriteObject implements ICollidableWithTil
 		setSpeed(rand.nextInt(movingX ? 4 : 3) + 1);
 	}
 
+	/**
+	 * SOURCE: <a href="https://stackoverflow.com/a/7228322">StackOverflow: Generate random number between two numbers
+	 * in JavaScript</a>
+	 *
+	 * @return
+	 */
+	protected int calculateTotalEssence() {
+		int min = this.rarity.getCalculationValue() - 1;
+		int max = this.rarity.getCalculationValue() * 2;
+
+		return (int) ( Math.random() * (max - min + 1) + min );
+	}
+
 	public void damage(int damage) {
-		hp = Math.max(hp - damage, 0);
-		if (hp == 0) FightForSalvation.getInstance().deleteGameObject(this);
+		this.hp = Math.max(this.hp - damage, 0);
+
+		if (this.hp == 0) {
+			int essenceToGive = this.calculateTotalEssence();
+			FightForSalvation instance = FightForSalvation.getInstance();
+
+			instance.getPlayer().addEssence(essenceToGive);
+			instance.deleteGameObject(this);
+		}
 	}
 
 	@Override
--- src/main/java/com/yologamer123415/fightforsalvation/usables/weapons/Weapon.java
+++ src/main/java/com/yologamer123415/fightforsalvation/usables/weapons/Weapon.java
@@ -1,16 +1,22 @@
 package com.yologamer123415.fightforsalvation.usables.weapons;
 
+import com.yologamer123415.fightforsalvation.helpers.Rarity;
+import com.yologamer123415.fightforsalvation.helpers.Vector;
 import com.yologamer123415.fightforsalvation.object.UsableObject;
 import nl.han.ica.oopg.objects.GameObject;
 import nl.han.ica.oopg.objects.Sprite;
 
 public abstract class Weapon extends UsableObject {
+	protected Rarity rarity;
 	protected GameObject holder;
 
 	public Weapon(String name, Sprite sprite, GameObject holder) {
 		super(name, sprite);
 		this.holder = holder;
+		this.rarity = Rarity.getRandomRarity();
 	}
+
+	public abstract void use(Vector mousePos);
 
 	@Override
 	public void update() {
