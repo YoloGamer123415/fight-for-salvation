8fa96a79d1d0128c7a6cae5f9bcf0a9aa6db7310
YoloGamer123415 <jorambuitenhuis@gmail.com>
2021-04-01 12:51:35 +0200

    Knife now should be able to deal damage to enemies

--- src/main/java/com/yologamer123415/fightforsalvation/helpers/LocationHelper.java
+++ src/main/java/com/yologamer123415/fightforsalvation/helpers/LocationHelper.java
@@ -1,6 +1,14 @@
 package com.yologamer123415.fightforsalvation.helpers;
 
+import com.yologamer123415.fightforsalvation.FightForSalvation;
 import com.yologamer123415.fightforsalvation.generators.MapGenerator;
+import com.yologamer123415.fightforsalvation.monsters.Monster;
+import com.yologamer123415.fightforsalvation.player.Player;
+import nl.han.ica.oopg.objects.GameObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 public class LocationHelper {
 	public static float screenToTilePixel(float pixelLocation) {
@@ -9,5 +17,23 @@ public class LocationHelper {
 
 	public static float tileToScreenPixel(float pixelLocation) {
 		return pixelLocation * MapGenerator.TILESIZE;
+	}
+
+	public static List<GameObject> findTargetsWithinRange(double range, GameObject aroundObject) {
+		if (aroundObject instanceof Monster || aroundObject instanceof Player) {
+			Vector<GameObject> gameObjects = FightForSalvation.getInstance().getGameObjectItems();
+			List<GameObject> hits = new ArrayList<>();
+			Class<? extends GameObject> target = aroundObject instanceof Player
+					? Monster.class
+					: Player.class;
+
+			for (GameObject ob : gameObjects)
+				if ( target.isInstance(ob) && ob.getDistanceFrom(aroundObject) <= range )
+					hits.add( ob );
+
+			return hits;
+		}
+
+		return new ArrayList<>();
 	}
 }
