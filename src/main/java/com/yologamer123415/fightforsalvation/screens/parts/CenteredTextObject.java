dcee72ca5b62cbdc3bdf1a8c3f2ec92e7d257097
SBDeveloper <stijnbannink40@gmail.com>
2021-04-05 16:49:12 +0200

    Moved CenteredTextObject to a general GameObject

--- src/main/java/com/yologamer123415/fightforsalvation/screens/parts/CenteredTextObject.java
+++ src/main/java/com/yologamer123415/fightforsalvation/screens/parts/CenteredTextObject.java
@@ -1,19 +1,31 @@
 package com.yologamer123415.fightforsalvation.screens.parts;
 
-import nl.han.ica.oopg.objects.TextObject;
+import nl.han.ica.oopg.objects.GameObject;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
-public class CenteredTextObject extends TextObject {
+public class CenteredTextObject extends GameObject {
+	private final String text;
+	private final int fontSize;
+	private int RGB = 0;
+
 	public CenteredTextObject(String text, int fontSize) {
-		super(text, fontSize);
+		this.text = text;
+		this.fontSize = fontSize;
+	}
+
+	public void setRGB(int RGB) {
+		this.RGB = RGB;
 	}
 
 	@Override
 	public void draw(PGraphics g) {
-		g.fill(0);
+		g.fill(RGB);
 		g.textAlign(PConstants.CENTER);
-		g.textSize(getFontSize());
-		g.text(getText(), this.x, this.y);
+		g.textSize(this.fontSize);
+		g.text(this.text, this.x, this.y);
 	}
+
+	@Override
+	public void update() {}
 }
